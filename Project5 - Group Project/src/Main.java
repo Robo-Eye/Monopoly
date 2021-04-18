899d0a6d42933205676f6e8d874e260bcd606bee
PLANKED20 <PLANKED20@5CG024DGCM.gcc.edu>
2021-04-18 15:05:45 -0400

    See if this works

--- Project5 - Group Project/src/Main.java
+++ Project5 - Group Project/src/Main.java
@@ -4,14 +4,15 @@
  * @author PLANKED20
  *
  */
+//See if this works...
+
 public class Main {
 	public static void main(String[] args) {
 
 		Board b = new Board();
 		b.numPlayers = 4;
+		
 
-//Testing die rolls
-// Testing123
 		// for (int i = 0; i < 10; i++) {
 		// System.out.println(b.rollDice());
 		// }
