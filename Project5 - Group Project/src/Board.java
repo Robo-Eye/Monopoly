debaa72515dae2bea4c6a7304e097b8616793f75
DECKERCJ20 <DECKERCJ20@5CG024DGSV.gcc.edu>
2021-04-29 20:34:33 -0400

    error fixing

--- Project5 - Group Project/src/Board.java
+++ Project5 - Group Project/src/Board.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
-//Board Test Comment
+//Board Test Comment.
 //Test comment to see if push is working
 //Pulls is working, is the push working?
 public class Board {
