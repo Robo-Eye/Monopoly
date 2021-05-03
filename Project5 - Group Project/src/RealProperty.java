b1dde1bf72046063ee51fc9d717851fe587635d1
DECKERCJ20 <DECKERCJ20@5CG024DGSV.gcc.edu>
2021-05-03 12:02:21 -0400

    fixed errors

--- Project5 - Group Project/src/RealProperty.java
+++ Project5 - Group Project/src/RealProperty.java
@@ -1,15 +1,57 @@
 public class RealProperty extends Property {
 	public int rent;
+	public int houseCount;
+	public int houseCost;
+	public int rent1house;
+	public int rent2house;
+	public int rent3house;
+	public int rent4house;
+	public int rentHotel;
 
 	//Constructor
-	public RealProperty(String name, int cost, int morgage, int rent) {
+	public RealProperty(String name, int cost, int morgage, int rent, int houseCost, int rent1house, int rent2house, int rent3house, int rent4house, int rentHotel) {
 	super(name, cost, morgage);
 		this.rent = rent;
+		this.houseCount = 0;
+		this.houseCost = houseCost;
+		this.rent1house = rent1house;
+		this.rent2house = rent2house;
+		this.rent3house = rent3house;
+		this.rent4house = rent4house;
+		this.rentHotel = rentHotel;
+	}
+
+	public int getRent1house() {
+		return rent1house;
+	}
+
+	public int getRent2house() {
+		return rent2house;
+	}
+
+	public int getRent3house() {
+		return rent3house;
+	}
+
+	public int getRent4house() {
+		return rent4house;
+	}
+
+	public int getRentHotel() {
+		return rentHotel;
 	}
 
 	//rent getter
 	public int getRent() {
 		return this.rent;
+	}
+
+	public int getHouseCount() {
+		return houseCount;
+	}
+
+	public void setHouseCount(int houseCount) {
+		this.houseCount = houseCount;
 	}
 
 
